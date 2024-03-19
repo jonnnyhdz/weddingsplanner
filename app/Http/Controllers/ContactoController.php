df93efa59522acc7f3eb9f385584d918e00ed684
jonnnyhdz <jonathanhdz262@gmail.com>
2024-03-19 13:44:28 -0500

    weddingsplanner

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contacto;
+use App\Http\Requests\StoreContactoRequest;
+use App\Http\Requests\UpdateContactoRequest;
+
+class ContactoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreContactoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Contacto $contacto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contacto $contacto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateContactoRequest $request, Contacto $contacto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contacto $contacto)
+    {
+        //
+    }
+}
